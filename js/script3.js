f5d79227ebfa84b3a76dc92c78e940c680f97e9d
Guillaume <guillaume@debeire.com>
2021-11-30 22:14:09 +0100

    stupid stuff'

--- js/script3.js
+++ js/script3.js
@@ -5,6 +5,31 @@ const title = document.getElementById('title');
 const white = document.getElementById('white');
 const white2 = document.getElementById('white2');
 
+const scrollElement = document.getElementById('scroll');
+
+const scrollAction = () => {
+    scrollElement.scrollTo({
+        top: 280,
+        behavior: 'smooth'
+    });
+}
+
+const scrollBack = () => {
+    scrollElement.scrollTo({
+        top: 0,
+    })
+}
+
+const smoothScroll = (h) => {
+    let i = h || 0;
+    if (i < 400) {
+      setTimeout(() => {
+        scrollElement.scrollTo(0, i);
+        smoothScroll(i + 0.15);
+      }, );
+    }
+  }
+
 const enhance = () => {
     title.classList.toggle('slide');
     img.classList.toggle('zoom')
@@ -14,6 +39,7 @@ const enhance = () => {
 
 card.addEventListener('mouseenter', () => {
     enhance();
+    smoothScroll();
 })
 card.addEventListener('mouseleave', () => {
     enhance();
